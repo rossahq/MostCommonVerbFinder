565e6208fc2fbf74b2122ec846bddfbd3fe9282c
rossahq <rossahq@gmail.com>
2019-10-05 14:02:40 +0100

    add pfd parsing

--- main/main.py
+++ main/main.py
@@ -1,7 +1,12 @@
+import io
 import pathlib
 import nltk
 
 from nltk.corpus.reader.plaintext import PlaintextCorpusReader
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
@@ -9,14 +14,13 @@ nltk.download('tagsets')
 
 class Main:
     def main(self):
+        path = r'C:\Users\ROSSA\PycharmProjects\MostCommonVerbFinder\documents'
+        filename = 'iraq_summary.pdf'
 
-        corpusdir = (r'C:\Users\ROSSA\PycharmProjects\MostCommonVerbFinder\new_corpus')  # Directory of corpus.
-
-        newcorpus = PlaintextCorpusReader(corpusdir, '.*')
-
-        data = pathlib.Path(r'C:\Users\ROSSA\PycharmProjects\CorpusCreator\new_corpus', 'file2.txt')
-        fp = open(data, 'r', encoding="utf8")
-        data = fp.read()
+        if 'pdf' in filename:
+            data = self.parse_pdf(path, filename)
+        else:
+            data = self.read_txt(path, filename)
 
         tokens = nltk.word_tokenize(data)
         tagged_tokens = nltk.pos_tag(tokens)
@@ -26,11 +30,39 @@ class Main:
         print(freq.most_common(20))
         #print(frequency)
 
-      #  print(nltk.help.upenn_tagset())
+        #print(nltk.help.upenn_tagset())
+
 
     def split_text(self, text):
         split_text = text.split(".")
         return split_text
+
+
+    def read_txt(self, path, filename):
+        data = pathlib.Path(path, filename)
+        fp = open(data, 'r', encoding="utf8")
+        data = fp.read()
+        return data
+
+
+    def parse_pdf(self, path, filename):
+        data = pathlib.Path(path, filename)
+        fp = open(data, 'rb')
+        rsrcmgr = PDFResourceManager()
+        retstr = io.StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        # Create a PDF interpreter object.
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        # Process each page contained in the document.
+
+        for page in PDFPage.get_pages(fp):
+            interpreter.process_page(page)
+            data = retstr.getvalue()
+
+        return data
+
 
 
 if __name__ == '__main__':
