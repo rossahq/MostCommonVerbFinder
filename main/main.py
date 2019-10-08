cca29cab7489052b34cee6afae6014acc2171b8a
rossahq <rossahq@gmail.com>
2019-10-08 01:45:41 +0100

    add text file support

--- main/main.py
+++ main/main.py
@@ -1,8 +1,9 @@
 import io
 import pathlib
 import nltk
+from nltk.corpus import stopwords
+from nltk import FreqDist
 
-from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
@@ -11,31 +12,39 @@ from pdfminer.pdfpage import PDFPage
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('tagsets')
+nltk.download('stopwords')
+
 
 class Main:
     def main(self):
         path = r'C:\Users\ROSSA\PycharmProjects\MostCommonVerbFinder\documents'
-        filename = 'iraq_summary.pdf'
+        filename = 'huttonreport.pdf'
 
         if 'pdf' in filename:
             data = self.parse_pdf(path, filename)
         else:
             data = self.read_txt(path, filename)
 
+        data = self.clean_pdf(data)
+
+        data = str(data)
+
         tokens = nltk.word_tokenize(data)
         tagged_tokens = nltk.pos_tag(tokens)
-        verbs = [token[0] for token in tagged_tokens if token[1] in ['VB', 'VBD', 'VBP', 'VBG']]
-        from nltk import FreqDist
+        verbs = [token[0] for token in tagged_tokens if token[1] in ['VB', 'VBD', 'VBP', 'VBG', 'VBN']]
         freq = FreqDist(verbs)
-        print(freq.most_common(20))
-        #print(frequency)
-
-        #print(nltk.help.upenn_tagset())
-
+        freq_verb = freq.most_common(50)
+        print(type(freq_verb))
+        print(freq_verb)
 
     def split_text(self, text):
         split_text = text.split(".")
         return split_text
+
+    def remove_stopwords(self, text):
+        stop_words = stopwords.words('english')
+        text = [w for w in text if w not in stop_words]
+        return text
 
 
     def read_txt(self, path, filename):
@@ -44,6 +53,9 @@ class Main:
         data = fp.read()
         return data
 
+    def clean_pdf(self, text):
+        text = text.replace("\n", " ")
+        return text.encode('ascii', 'ignore')
 
     def parse_pdf(self, path, filename):
         data = pathlib.Path(path, filename)
